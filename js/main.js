7357b52c2b7f959de258c299f9cf96482ead8833
Nikos Andriopoulos <nikos.andriopoulos05@icloud.com>
2026-06-10 00:54:35 +0300

    Addition of TMDB API for Trending Now Section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Theme Toggle Functionality
+/* Theme Toggle Functionality */
 
 //Select Theme Toggle Button
 const themeToggleBtn = document.querySelector(".theme-toggle-btn");
@@ -47,7 +47,74 @@ themeToggleBtn.addEventListener("click", () => {
 
     }
 
-}); 
+});
+
+
+/* TMDB API */
+
+const API_KEY = "41a0584a9b46ca755d88d78ba66313e1";
+const BASE_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`;
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const movieContainer = document.querySelector(".movie-grid");
+
+async function fetchTrendingMovies() {
+
+    try {
+
+        const response = await fetch(BASE_URL);
+
+        const data = await response.json();
+
+        displayMovies(data.results);
+
+    }
+
+    catch(error) {
+
+        console.log("Error fetching movies:", error);
+
+    }
+
+}
+
+function displayMovies(movies) {
+
+    movieContainer.innerHTML = "";
+    movies.slice(0,8).forEach(movie => {
+
+        const movieCard = document.createElement("div");
+        movieCard.classList.add("movie-card");
+        movieCard.innerHTML = `
+        
+            <img src="${IMAGE_URL + movie.poster_path}"
+                 alt="${movie.title}">
+
+            <div class="movie-info">
+                
+                 <h3>${movie.title}</h3>
+
+                 <p>⭐ ${movie.vote_average.toFixed(1)}</p>                
+            
+            </div>        
+        `;
+
+        movieContainer.appendChild(movieCard);
+
+    });
+
+}
+
+fetchTrendingMovies();
+
+
+
+
+
+
+
+    
+
+
 
 
 
